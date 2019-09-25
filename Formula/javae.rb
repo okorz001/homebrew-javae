efec0eab8d6da960e38ccbe11ebd705a3ec5b844
Oscar Korz <okorz001@gmail.com>
2019-09-24 19:43:12 -0700

    Initial commit

---
+++ Formula/javae.rb
@@ -0,0 +1,17 @@
+class Javae < Formula
+  desc "compile and execute one-line Java programs"
+  homepage "https://okorz001.github.io/javae/"
+  head "https://github.com/okorz001/javae.git"
+
+  depends_on :java
+  depends_on "groff" => :optional
+  depends_on "bats-core" => :test
+
+  def install
+    system "make", "PREFIX=#{prefix}", "install"
+  end
+
+  test do
+    system bin/"javae" 'System.out.println("Hello world!");'
+  end
+end
